eb4c530d59ce77974b60d66b4abcbc965aeaec6a
boychaboy <hoon2j@gmail.com>
2021-04-26 23:39:46 +0900

    feat: template experimenting

--- scripts/run_rte.sh
+++ scripts/run_rte.sh
@@ -2,7 +2,7 @@ OUTPUT_DIR=$1
 export TASK_NAME=rte
 
 CUDA_VISIBLE_DEVICE=$2 python text-classification/run_glue.py \
-  --model_name_or_path roberta-large \
+  --model_name_or_path albert-xxlarge-v2 \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
