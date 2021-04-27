e79266510f8d56cdc7da2e8c788bf5144e37fdac
boychaboy <hoon2j@gmail.com>
2021-04-27 17:14:44 +0900

    template result : type1

--- crows-pairs/run_metric.sh
+++ crows-pairs/run_metric.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICE=$1 python metric.py \
     --input_file data/crows_pairs_anonymized.csv \
-    --lm_model albert \
-    --output_file albert-xxlarge-v1
+    --lm_model bert-large \
+    --output_file bert-large-cased
